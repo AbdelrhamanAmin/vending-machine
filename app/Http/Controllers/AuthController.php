b0e8491830e7c62a089909ab8b966e5537eccedb
AbdelrahmanAmin <abdelrahman.amin93@gmail.com>
2022-04-03 02:54:50 +0200

    Add auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => ['required','unique:users,username'],
+            'password' => ['required'],
+            'role'     => ['required',Rule::in(USER::USER_ROLES)]
+        ]);
+
+        if($validator->fails()){
+            return $this->handleError($validator->errors(), 'Validation Error', 400);
+        }
+
+        $user = User::create([
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+        $data['token'] =  $user->createToken('auth_token')->plainTextToken;
+        $data['username'] =  $user->username;
+        return $this->handleResponse($data, ' registered!');
+    }
+
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['username' => $request->username, 'password' => $request->password])){
+            $auth = Auth::user();
+            $success['token'] =  $auth->createToken('auth_token')->plainTextToken;
+            $success['name'] =  $auth->name;
+
+            return $this->handleResponse($success, 'User successfully logged-in');
+        }
+        else{
+            return $this->handleError('Unauthorized.', ['error'=>'Unauthorized']);
+        }
+    }
+
+
+    /**
+     * Log the user out
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        return $this->handleResponse( auth()->user()->tokens()->delete(), 'User successfully logged-out');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    const BUYER_ROLE = 'buyer';
+    const SELLER_ROLE = 'seller';
+    const USER_ROLES = [self::BUYER_ROLE, self::SELLER_ROLE];
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +23,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
+        'role',
         'password',
+        'deposit'
     ];
 
     /**
@@ -30,15 +36,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
