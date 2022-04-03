904600bbbbe10f16688e376774522ac78c5c9dcc
AbdelrahmanAmin <abdelrahman.amin93@gmail.com>
2022-04-03 03:04:06 +0200

    Add base controller response handler methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,27 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public function handleResponse($data, $msg = null, $code = 200)
+    {
+    	$response = [
+            'success' => true,
+            'data'    => $data,
+            'message' => $msg,
+        ];
+        return response()->json($response, $code);
+    }
+
+    public function handleError($error, $errorMsg = [], $code = 404)
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if(!empty($errorMsg)){
+            $response['data'] = $errorMsg;
+        }
+        return response()->json($response, $code);
+    }
 }
